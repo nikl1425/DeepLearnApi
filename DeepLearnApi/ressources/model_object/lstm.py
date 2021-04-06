bae69c940acc5f1f5d249767b69e5b19111ad2e8
Niklas Hjort <niklas@astne.dk>
2021-04-06 19:47:40 +0200

    hej

--- DeepLearnApi/ressources/model_object/lstm.py
+++ DeepLearnApi/ressources/model_object/lstm.py
@@ -1,5 +1,26 @@
+from model import Model
+import numpy as np
+import pandas as pd
 
 
-class lstm:
-    def __init__(self):
+class lstm(Model):
+    def __init__(self, model_name, database_name):
+        super().__init__(model_name, database_name)
+        self.database_name = database_name
+
+    def reformat_data(self):
+        # Load dataset from mySql
+        dataFrame = self.load_data(self.database_name)
+
+        # Select and adding columns for log and raw percentage change between each datapoint
+        dataFrame = dataFrame[['high', 'low', 'close']]
+        dataFrame['returns'] = dataFrame.close.pct_change()
+
+        print(dataFrame.close)
+
+    def train_model(self):
         pass
+
+
+obj = lstm("apple", 'apple')
+obj.reformat_data()
