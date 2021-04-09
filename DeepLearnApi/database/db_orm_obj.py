54fdf07c9ce7a7fa0f4fc2ee9b8ff55a030a4679
nikl1425 <niklas@astne.dk>
2021-04-09 20:32:33 +0200

    ORM underway

---
+++ DeepLearnApi/database/db_orm_obj.py
@@ -0,0 +1,33 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class StockData(Base):
+    __tablename__ = 'stock_data'
+
+    id = Column(Integer, primary_key=True)
+    datetime = Column(String)
+    open = Column(String)
+    high = Column(String)
+    low = Column(String)
+    close = Column(String)
+    volume = Column(String)
+    stock_id = Column(Integer)
+
+    def __repr__(self):
+        return "<Stockdata(id='%s', datetime='%s' open='%s', high='%s', low='%s', close='%s', volume='%s', stock_id='%s')>" % (
+            self.id, self.datetime, self.open, self.high, self.low, self.close, self.volume, self.stock_id
+        )
+
+class StockType(Base):
+    __tablename = 'stock_type'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    stock_name = Column(String)
+
+    def __repr__(self):
+        return "<StockType(id='%s',"
+
--- DeepLearnApi/database/mysql_connector.py
+++ DeepLearnApi/database/mysql_connector.py
@@ -3,29 +3,39 @@ import pandas as pd
 from sqlalchemy import create_engine
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
+from sqlalchemy.orm import sessionmaker
+from database.db_orm_obj import StockData
 
-# update 08-04
-
+# API fetch key
 API_KEY = "6a81f55d739d49c2a19610cd4a98e366"
 
+# connection engine
 engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}".format(
     user="root",
-    pw="Drageild07",
-    db="Lstm"
+    pw="Nvp92agn",
+    db="stock",
+    echo=True # Logging
 ))
-last = datetime.now() - relativedelta(years=5)
-last = last.strftime("%Y-%m-%d")
-print(last)
+
+# object relation mapping object
+stock_data = StockData
+
 
 def fetch_from_api(name):
     start_date = (datetime.now() - relativedelta(years=14)).strftime("%Y-%m-%d")
     end_date = datetime.today().strftime("%Y-%m-%d")
     close_time = "2021:24:00"
+    print("start:  "+ str(start_date) + "  end :  " + str(end_date))
     data = requests.get(f"https://api.twelvedata.com/time_series?symbol={name}&interval=1day&type=stock&format=JSON&start_date={start_date}%{close_time}&end_date={end_date}%{close_time}&apikey={API_KEY}")
     print("SUCCESS")
     return data.json()
 
+
+def get_all_stock_types():
+    pass
+
 def update_apple():
+    stock_name = "AAPL"
     request = fetch_from_api("AAPL")
     request = request["values"]
     request = pd.DataFrame(request)
@@ -33,6 +43,14 @@ def update_apple():
     try:
         request.to_sql("apple", con=engine, if_exists='replace', chunksize=1000, index=False)
         print("appended")
-    except:
-        print("could not append or maybe connect")
+    except ValueError:
+        print("could not append or maybe connect" + str(ValueError))
 update_apple()
+
+
+Session = sessionmaker(bind = engine)
+session = Session()
+result = session.query(stock_data).all()
+
+for row in result:
+    print("name: ", row.open)
